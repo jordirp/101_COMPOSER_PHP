2cbe93519d8a6301a3737ef5f9caed5256885b8c
jordirp <jordirodriguez2@iesebre.com>
2021-10-18 19:11:36 +0200

    fet hasta lo pati

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6718b7f8a7575f65c633347cd8ee92823d25cf5c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6718b7f8a7575f65c633347cd8ee92823d25cf5c',
+            'dev_requirement' => false,
+        ),
+        'fzaninotto/faker' => array(
+            'pretty_version' => 'v1.9.2',
+            'version' => '1.9.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fzaninotto/faker',
+            'aliases' => array(),
+            'reference' => '848d8125239d7dbf8ab25cb7f054f1a630e68c2e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
--- composer/vendor/fzaninotto/index.php
+++ composer/vendor/fzaninotto/index.php
@@ -4,3 +4,7 @@ require_once "vendor/autoload.php";
 $faker = Faker\Factory::create();
 
 echo $faker->name;
+
+use Carbon\Carbon;
+
+printf("Now: %s", Carbon::now());
